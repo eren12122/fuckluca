a0f84e2645c3236e20bc73a15c73077e4e17cbca
eren12122 <44553580+eren12122@users.noreply.github.com>
2018-11-26 20:27:19 +0300

    Add files via upload

---
+++ komutlar/steamid.js
@@ -0,0 +1,28 @@
+const Discord = require('discord.js');
+
+exports.run = (client, message, args) => {
+	let mesaj = args.slice(0).join(' ');
+  if (mesaj.length < 1) return message.reply('Yazmam İçin Birşey Yazmalısın! :unlem: ');
+  
+    message.delete();
+    const embed = new Discord.RichEmbed()
+    .setAuthor(message.author.username + `Steam ID`)
+    .setColor("#18BF7E")
+    .setDescription(`https://steamcommunity.com/id${mesaj}/`)
+    .setThumbnail('')
+    return message.channel.sendEmbed(embed);
+};
+
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['steamid', 'steam'],
+  permLevel: 2
+};
+
+exports.help = {
+  name: 'steamid',
+  description: 'steamid.',
+  usage: 'steamid'
+};
